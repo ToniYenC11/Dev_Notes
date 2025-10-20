210c3191917cb93f51a984cbd55d97e836347816
ToniYenC11 <tonicastanares11@gmail.com>
2025-10-20 19:42:56 +0800

    Cotton eye joe

--- Software_engineering_notes/Data Structures and Algorithms and Function/Data_Structures/Stacks_Queues.py
+++ Software_engineering_notes/Data Structures and Algorithms and Function/Data_Structures/Stacks_Queues.py
@@ -5,34 +5,3 @@ queue.append(4)
 
 print(queue)
 
-# Implementing queue from scratch
-
-class Node:
-    def __init__(self, data, next=None):
-        self.data = data 
-        self.next = next
-
-class Queue:
-    def __init__(self, head:Node, tail:Node=None):
-        self.head = head 
-        self.tail = tail
-    
-    def enqueue(self,node):
-        if self.head is None:
-           self.head = node
-        else:
-            self.tail.next = node
-        self.tail = node
-    
-    def dequeue(self):
-        if self.head:
-            current_node = self.head
-            self.head = self.head.next 
-            current_node.next = None 
-            return current_node
-        if self.head is None:
-            return None
-    
-    
-    
-    
